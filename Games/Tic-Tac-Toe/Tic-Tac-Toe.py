19e30a2c94713e676c88b08554ab88665326c37c
우정연 <kikanaidek@gmail.com>
2022-05-07 21:07:07 +0900

    Update Tic-Tac-Toe.py

--- Games/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ Games/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -16,6 +16,20 @@ class Main:
         self.turn = True
         self.done = False
 
+        for i in range(3):
+            self.matrix.append([])
+            for j in range(3):
+                self.matrix[i].append(Button(frame, image=self.eImage, text=' ',
+                command= lambda row=i, col=j: self.pressed(row, col)))
+                self.matrix[i][j].grid(row=i, column=j)
+        
+        self.explain = StringVar()
+        self.explain.set('플레이어 X 차례')
+        Label(window, textvariable=self.explain).pack()
+        Button(window, text='다시 시작', command=self.refresh).pack()
+
         window.mainloop()
+        
+    
 
 Main()
