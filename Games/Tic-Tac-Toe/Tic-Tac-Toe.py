28cf936e7f468bc1b77a5718623328a24b54299b
우정연 <kikanaidek@gmail.com>
2022-05-07 21:02:22 +0900

    main gui

--- Games/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ Games/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import Turtle
 
 class Main:
     def __init__(self):
@@ -11,22 +12,9 @@ class Main:
         self.xImage = PhotoImage(file='image/x.gif')
         self.oImage = PhotoImage(file='image/o.gif')
         self.eImage = PhotoImage(file='image/empty.gif')
-
-        self.cells = []     # 3x3 cell
+        self.matrix = []
         self.turn = True
         self.done = False
-
-        for i in range(3):
-            self.cells.append([])
-            for j in range(3):
-                self.cells[i].append(Button(frame, image = self.eImage, \
-                text= ''))
-                self.cells[i][j](row=i, column=j)
-
-        self.explain = StringVar()
-        self.explain.set('플레이어 X 차례')
-        Label(window, textvariable=self.explain).pack()
-        Button(window, text='다시 시작').pack()
 
         window.mainloop()
 
