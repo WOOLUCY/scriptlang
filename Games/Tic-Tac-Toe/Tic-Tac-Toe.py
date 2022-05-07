14ae4b15a0b897f0e4031447a03d9b9d4ae16bd1
우정연 <kikanaidek@gmail.com>
2022-05-07 21:34:56 +0900

    Update Tic-Tac-Toe.py

--- Games/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ Games/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -1,76 +1,77 @@
 from tkinter import *
-from turtle import Turtle
 
-class Main:
-    def __init__(self):
-        window = Tk()
-        window.title('Tic-Tac-Toe')
+class TicTacToe:
+    def __init__(self):         # initiating window
+        tk = Tk()
 
-        frame = Frame(window)
+        frame = Frame(tk)
         frame.pack()
-
-        self.xImage = PhotoImage(file='image/x.gif')
-        self.oImage = PhotoImage(file='image/o.gif')
-        self.eImage = PhotoImage(file='image/empty.gif')
-        self.matrix = []
+        
+        # image load
+        self.xImage = PhotoImage(file='image/x.gif')        # X image
+        self.oImage = PhotoImage(file='image/o.gif')        # O image
+        self.eImage = PhotoImage(file='image/empty.gif')    # empty image
+        
+        self.cells = []
         self.turn = True
         self.done = False
 
+        # cells
         for i in range(3):
-            self.matrix.append([])
+            self.cells.append([])
             for j in range(3):
-                self.matrix[i].append(Button(frame, image=self.eImage, text=' ',
-                command= lambda row=i, col=j: self.pressed(row, col)))
-                self.matrix[i][j].grid(row=i, column=j)
+                self.cells[i].append(Button(frame, image=self.eImage, text=' ', command= lambda row=i, col=j: self.pressed(row, col)))
+                self.cells[i][j].grid(row=i, column=j)
         
-        self.explain = StringVar()
-        self.explain.set('플레이어 X 차례')
-        Label(window, textvariable=self.explain).pack()
-        Button(window, text='다시 시작', command=self.refresh).pack()
+        # result
+        self.result = StringVar()
+        self.result.set('X 차례')
 
-        window.mainloop()
+        Label(tk, textvariable=self.result).pack()
+
+        tk.mainloop()
 
     def pressed(self, row, col):
-        if not self.done and self.matrix[row][col]['text'] == ' ':
+        if not self.done and self.cells[row][col]['text'] == ' ':
             if self.turn:
-                self.matrix[row][col]['image'] = self.xImage
-                self.matrix[row][col]['text'] = 'X'
+                self.cells[row][col]['image'] = self.xImage
+                self.cells[row][col]['text'] = 'X'
             else:
-                self.matrix[row][col]['image'] = self.oImage
-                self.matrix[row][col]['text'] = 'O'
+                self.cells[row][col]['image'] = self.oImage
+                self.cells[row][col]['text'] = 'O'
 
             self.turn = not self.turn
 
             if self.check() == '@':
-                self.explain.set('비겼습니다.')
+                self.result.set('비김! 게임이 끝났습니다')
             elif self.check() != ' ':
-                self.explain.set(self.check()+'가 이겼습니다.')
+                self.result.set(self.check()+' 승리! 게임이 끝났습니다')
                 self.done = True
             elif self.turn:
-                self.explain.set('플레이어 X 차례')
+                self.result.set('X 차례')
             else:
-                self.explain.set('플레이어 O 차례')
+                self.result.set('O 차례')
 
     
     def check(self):
         for i in range(3):
-            ch = self.matrix[i][0]['text']
-            if ch != ' ' and ch == self.matrix[i][1]['text'] and ch == self.matrix[i][2]['text']:
+            ch = self.cells[i][0]['text']
+            if ch != ' ' and ch == self.cells[i][1]['text'] and ch == self.cells[i][2]['text']:
                 return ch
-            ch = self.matrix[0][i]['text']
-            if ch != ' ' and ch == self.matrix[1][i]['text'] and ch == self.matrix[2][i]['text']:
+            ch = self.cells[0][i]['text']
+            if ch != ' ' and ch == self.cells[1][i]['text'] and ch == self.cells[2][i]['text']:
                 return ch
         
-        ch = self.matrix[1][1]['text']
-        if ch != ' ' and ch == self.matrix[0][0]['text'] and ch == self.matrix[2][2]['text']:
+        ch = self.cells[1][1]['text']
+        if ch != ' ' and ch == self.cells[0][0]['text'] and ch == self.cells[2][2]['text']:
             return ch
-        if ch != ' ' and ch == self.matrix[0][2]['text'] and ch == self.matrix[2][0]['text']:
+        if ch != ' ' and ch == self.cells[0][2]['text'] and ch == self.cells[2][0]['text']:
             return ch
         
         flag = True
         for i in range(3):
             for j in range(3):
-                if self.matrix[i][j]['text'] == ' ':
+                if self.cells[i][j]['text'] == ' ':
                     flag = False
                     break
             if flag == False:
@@ -83,10 +84,10 @@ class Main:
     def refresh(self):
         self.turn = True
         self.done = False
-        self.explain.set('플레이어 X 차례')
+        self.result.set('X 차례')
         for i in range(3):
             for j in range(3):
                 self.matrix[i][j]['image'] = self.eImage
                 self.matrix[i][j]['text'] = ' '
             
-Main()
+TicTacToe()
