e22799788ddc509a170fe6fc866d902f5a6873b8
우정연 <kikanaidek@gmail.com>
2022-05-17 11:52:03 +0900

    add scrollbar

--- TermProject/Source/where_hospital.py
+++ TermProject/Source/where_hospital.py
@@ -8,7 +8,7 @@ window.geometry("800x600+450+200")
 window.resizable(False, False)
 window.configure(bg='white')
 
-searchImage = PhotoImage(file='image/search.png')    # search image
+searchImage = PhotoImage(file='image/search.png')  # search image
 emailImage = PhotoImage(file='image/mail.png')     # gmail image
 mapImage = PhotoImage(file='image/map.png')        # naver map image
 logoImage = PhotoImage(file='image/logo.png')      # logo image
@@ -18,13 +18,18 @@ def InitScreen():
 
     # 시(군) 선택 부분
     global CityListBox
-    CityListBox = Listbox(window, activestyle='none',relief='ridge', font=fontNormal) 
-    slist = ["안양시", "시흥시", "성남시", "광명시"]
+    CityScrollbar = Scrollbar(window)
+    CityListBox = Listbox(window, activestyle='none',relief='ridge', font=fontNormal, yscrollcommand=CityScrollbar.set) 
+    slist = ["안양시", "시흥시", "성남시", "광명시"]        # todo: add list
     for i, s in enumerate(slist): 
         CityListBox.insert(i, s)
     CityListBox.place(x=110, y=10, width=280, height=70)
 
+    CityScrollbar.config(command=CityListBox.yview) 
+    CityScrollbar.place(x=390, y=10, width=20, height=70)
+
     # 병원종류
+    # todo: 해당 내용 검색창으로 변경
     global TypeListBox
     TypeListBox = Listbox(window, activestyle='none', relief='ridge', font=fontNormal)
     slist = ["한방병원", "종합병원", "요양병원"]
@@ -34,11 +39,15 @@ def InitScreen():
 
     # 진료 과목 내용 정보
     global DeptListBox
-    DeptListBox = Listbox(window, activestyle='none', relief='ridge', font=fontNormal) 
-    slist = ["치과", "내과", "피부과", "정형외과"]
+    DeptScrollbar = Scrollbar(window)
+    DeptListBox = Listbox(window, activestyle='none', relief='ridge', font=fontNormal, yscrollcommand=DeptScrollbar.set) 
+    slist = ["치과", "내과", "피부과", "정형외과"]          # todo: add list
     for i, s in enumerate(slist): 
         DeptListBox.insert(i, s)
     DeptListBox.place(x=110, y=170, width=280, height=70) 
+
+    DeptScrollbar.config(command=DeptListBox.yview) 
+    DeptScrollbar.place(x=390, y=170, width=20, height=70)
 
     # 목록 부분
     global listBox
@@ -66,7 +75,6 @@ def InitScreen():
     SearchButton = Button(window, image=searchImage, bg="white")
     SearchButton.place(x=410, y=90, width=120, height=120)
     
-
     # 메일 부분
     global MailButton
     MailButton = Button(window, image=emailImage, bg="white")
