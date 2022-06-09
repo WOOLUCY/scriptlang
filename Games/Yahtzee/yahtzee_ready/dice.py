4f8ee7a8031d46afad954f20cbb0878fb467f6ae
우정연 <kikanaidek@gmail.com>
2022-06-09 19:49:50 +0900

    Update dice.py
    dice.py 작성

--- Games/Yahtzee/yahtzee_ready/dice.py
+++ Games/Yahtzee/yahtzee_ready/dice.py
@@ -2,7 +2,7 @@ import random
 
 class Dice:
     def rollDie(self):
-        self.roll = random.randint(1, 6)
+        self.roll = random.randint(1, 6)    # 1~6까지의 랜덤 숫자 나오게 함
 
     def getRoll(self):
         return self.roll
