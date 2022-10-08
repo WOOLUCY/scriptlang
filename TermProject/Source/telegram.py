a37c3c7bf6f6c1ae4d02df0577a3c9c9ca476070
WooJeongYeon <89962345+WOOLUCY@users.noreply.github.com>
2022-10-08 16:30:00 +0900

    Update telegram.py
    텔레그램 정보 가림

--- TermProject/Source/telegram.py
+++ TermProject/Source/telegram.py
@@ -16,8 +16,8 @@ def sendSelectedInfo():     # 선택된 병원 정보를 텔레그램으로 보�
     if server.hospital_name == None: 
         msgbox.showinfo("알림", "목록에서 병원을 먼저 선택해주십시오.")
         return
-    bot = telepot.Bot("5441441415:AAEolADNSY5sQlqcWJQCddqyQltgdB1hDh4")
-    bot.sendMessage('5554103153', server.info_text + '\n\n' + '[검색결과]' + '\n' +'https://www.google.com/search?q=' + server.hospital_name)
+    bot = telepot.Bot("")
+    bot.sendMessage('', server.info_text + '\n\n' + '[검색결과]' + '\n' +'https://www.google.com/search?q=' + server.hospital_name)
     msgbox.showinfo("알림", "메시지를 성공적으로 보냈습니다.")
 
 if __name__ == '__main__':
